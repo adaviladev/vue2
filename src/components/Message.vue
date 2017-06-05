73555bff86d80ef7dbed0e545cb52080db1a6243
adrian <adavila.dev@gmail.com>
2017-06-05 03:31:20 -0500

    First commit

---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box is-info">
+	  <p>
+		  <slot></slot>
+	  </p>
+  </div>
+  <!-- /.box is-info -->
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+	.box {
+		background: #e3e3e3;
+		padding: 10px;
+		border: 1px solid #c5c5c5;
+	}
+</style>
